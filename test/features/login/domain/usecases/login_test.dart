096faf7957bd1e37c34915e7a2b3a8b5b46c6d27
Vivek Kirpan <kirpan.vivek@gmail.com>
2021-06-25 19:10:12 +0530

    login test fixed

--- test/features/login/domain/usecases/login_test.dart
+++ test/features/login/domain/usecases/login_test.dart
@@ -1,26 +1,28 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_placed/features/login/domain/repositories/login_repository.dart';
 import 'package:get_placed/features/login/domain/usecases/login_usecase.dart';
+import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:dartz/dartz.dart';
 import 'package:get_placed/features/login/domain/entites/LoginEntities.dart';
+import 'login_test.mocks.dart';
 
 class MockLoginRepository extends Mock implements LoginRepository {}
 
+@GenerateMocks([MockLoginRepository])
 void main() {
   late MockLoginRepository mockLoginRepository;
   late Login usecase;
   setUp(() {
-    mockLoginRepository = MockLoginRepository();
+    mockLoginRepository = MockMockLoginRepository();
     usecase = Login(mockLoginRepository);
   });
 
   final tLogin = LoginEntities(login: "login", password: "password");
   test('use case call repository', () async {
     //given
-    var any2 = any;
     when(
-      mockLoginRepository.login(any2),
+      mockLoginRepository.login(tLogin),
     ).thenAnswer((_) async => Right(true));
     //when
     final result = await usecase.call(Params(login: tLogin));
